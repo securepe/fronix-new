0d681899f60acba7e2ed5273137c08954df46299
securepe <79696151+securepe@users.noreply.github.com>
2022-01-07 17:25:46 +0530

    Update contact.form.js

--- assets/js/contact.form.js
+++ assets/js/contact.form.js
@@ -1,14 +1,4 @@
-/**
-*
-* -----------------------------------------------------------------------------
-*
-* Template : Braintech - IT Solutions and Technology Startup HTML Template
-* Author : rs-theme
-* Author URI : http://www.rstheme.com/
-*
-* -----------------------------------------------------------------------------
-*
-**/
+
 
 (function($) {
     'use strict';
@@ -36,12 +26,19 @@
             // Make sure that the formMessages div has the 'success' class.
             $(formMessages).removeClass('error');
             $(formMessages).addClass('success');
+            $(formMessages).addClass('green_msg');
             
             var thank = $(response).find('h1:first')[0].outerHTML;
             var msg = $(response).find('p:first')[0].outerHTML;
-            alert(msg);
+            
+            //alert(msg);
+            
+            var middle_msg = msg.replace("<p class=\"card-p\">","");
+            var temp_msg = middle_msg.replace("</p>","");
+            var final_msg = temp_msg.replace("<p>","");
+            //alert(final_msg);
             // Set the message text.
-            $(formMessages).text(msg);
+            $(formMessages).text(final_msg);
 
             // Clear the form.
             $('#name, #email, #phone, #subject, #website, #message').val('');
@@ -50,10 +47,16 @@
             // Make sure that the formMessages div has the 'error' class.
             $(formMessages).removeClass('success');
             $(formMessages).addClass('error');
+            $(formMessages).removeClass('green_msg');
+            $(formMessages).addClass('red_msg');
 
             // Set the message text.
             if (data.responseText !== '') {
-                $(formMessages).text(data.responseText);
+                
+                var msg = $(data.responseText).find('p:first')[0].outerHTML;
+                var temp_msg = msg.replace("</p>","");
+                var final_msg = temp_msg.replace("<p>","");
+                $(formMessages).text(final_msg);
             } else {
                 $(formMessages).text('Oops! An error occured and your message could not be sent.');
             }
